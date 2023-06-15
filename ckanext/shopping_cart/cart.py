a8342564c246372b6a58e0c57f33dd99de7ec99e
Shilo Banihit <shiloworks@gmail.com>
2023-06-15 12:13:39 +1000

    Added support for storing cart data in plugin_extra_details.

--- ckanext/shopping_cart/cart.py
+++ ckanext/shopping_cart/cart.py
@@ -8,17 +8,22 @@ from typing import Any, Iterable, Optional, OrderedDict
 import ckan.lib.redis as redis
 import ckan.plugins.toolkit as tk
 from ckan.common import session
+from ckan.common import g
+import ckan.lib.helpers as h
+import json
+import ckan.logic as logic
 
 CONFIG_CART_FACTORY = "ckanext.shopping_cart.factory.default"
 DEFAULT_CART_FACTORY = "redis"
 
 
-def get_cart(scope: str, context: dict[str, Any]):
+def get_cart(scope: str, context: dict[str, Any], data_dict):
     factory = factories[
         tk.config.get(CONFIG_CART_FACTORY, DEFAULT_CART_FACTORY)
     ]
     cart = factory()
 
+    cart.data_dict = data_dict
     cart.identify(scope, context)
     return cart
 
@@ -32,6 +37,7 @@ class Item:
 class Cart(abc.ABC):
     id: Optional[str] = None
     content: Any
+    data_dict: Any
 
     def __init__(self):
         self.clear()
@@ -114,6 +120,61 @@ class SessionCart(Cart):
         self.session.pop(f"shopping_cart:{key}", None)
 
 
+class UserCart(Cart):
+    def __init__(self):
+        super().__init__()
+        self.site_user = tk.get_action("get_site_user")({'ignore_auth': True}, {})["name"]
+        self.admin_ctx = {"ignore_auth": True, "user": self.site_user }
+
+    def identify(self, scope: str, context: dict[str, Any]):
+        self.scope = scope
+        self.username = self.data_dict['__extras']['username']
+        self.id = self.username
+
+    def get_user(self):
+        # Default to own user
+        username = g.userobj.name
+        # Only admins can view another user's cart
+        if g.userobj.name != self.username:
+            if g.userobj.sysadmin is True:
+                username = self.username
+
+        user_id = { "id": username, "include_plugin_extras": True }
+        user = tk.get_action('user_show')(self.admin_ctx, user_id)
+        return user
+
+    def restore(self, key: str):
+        user = self.get_user()
+        plugin_extras = self.get_plugin_extras(user)
+        self.content = self.get_cart(plugin_extras, key)
+
+    def save(self, key: str):
+        user = self.get_user()
+        plugin_extras = self.get_plugin_extras(user)
+        plugin_extras[self.get_cart_key(key)] = self.content
+        user['plugin_extras'] = plugin_extras
+        tk.get_action('user_update')(self.admin_ctx, user)
+
+    def drop(self, key: str):
+        self.clear()
+        self.save(key)
+    
+    def get_plugin_extras(self, user):
+        if 'plugin_extras' in user and user.get('plugin_extras') is not None:
+            return user.get('plugin_extras')
+        else:
+            return {}
+    
+    def get_cart(self, plugin_extras, cart_name):
+        cart_key = self.get_cart_key(cart_name)
+        if plugin_extras is not None and cart_key in plugin_extras: 
+            return plugin_extras[cart_key]
+        else:
+            return {}      
+
+    def get_cart_key(self, key):
+        return f"shopping_cart__{key}"
+
 class FakeSessionCart(SessionCart):
     def __init__(self):
         super().__init__()
@@ -126,4 +187,5 @@ class FakeSessionCart(SessionCart):
 factories = {
     "redis": RedisCart,
     "session": SessionCart,
+    "user_plugin_extras": UserCart,
 }
